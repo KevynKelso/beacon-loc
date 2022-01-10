145fbb55fecaa5581d27f069ff612d1bee5909d9
Kevyn Kelso <vyn@Kevyns-MacBook-Pro.local>
2022-01-10 16:01:04 -0700

    chore: test pre-commit hook

--- script/install-hooks.sh
+++ script/install-hooks.sh
@@ -2,6 +2,7 @@
 
 GIT_DIR=$(git rev-parse --git-dir)
 
+
 echo "Installing hooks..."
 # this command creates symlink to our pre-commit script
 ln -s ../../scripts/pre-commit.sh $GIT_DIR/hooks/pre-commit
